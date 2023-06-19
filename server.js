4a5b7df4e73672b72e176d9072935495c2d09190
Daniel Berbel <daberbel@hotmail.com>
2023-06-19 10:22:44 +0200

    added final comments

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require("express");
 // iniciar express
 const app = express();
 
+// ruta inicial
 app.get("/", (req, res) => {
   res.send({ message: "Hello", change: "UNIR" }).status(200);
 });
 
+// ruta sumar
 app.get("/add/:number/:additor", (req, res) => {
   const result = parseInt(req.params.number, 10) + parseInt(req.params.additor, 10);
   res.send({ result }).status(200);
 });
 
+// ruta restar
 app.get("/substract/:number/:substractor", (req, res) => {
   const result = parseInt(req.params.number, 10) - parseInt(req.params.substractor, 10);
   res.send({ result }).status(200);
